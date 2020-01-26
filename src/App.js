62ede993baae824a98a59530e7c9d0af43eaf066
baumannalexj <baumannalexj@gmail.com>
2020-01-26 14:29:16 -0600

    working

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {Math.round((new Date()).getTime() / 1000)} Mmms today.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Get cookin'!
         </a>
       </header>
     </div>
